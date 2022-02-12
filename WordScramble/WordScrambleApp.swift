91a03a1e4e9bbbba29f49e7c0219ae6af4a0ebce
Jae Cho <jjaecho@gmail.com>
2022-02-12 00:29:01 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jae Cho on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
